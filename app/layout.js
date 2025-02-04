9602755753ec24e7413653863cc31f2502f76b2c
Vikas <vikas.kumar.singh1198@gmail.com>
2025-02-04 12:46:35 +0530

    Ready for partial deploy

--- app/layout.js
+++ app/layout.js
@@ -18,10 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth ">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
-      >
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden 
+       
+        `} >
         {children}
       </body>
     </html>
